d2e78658e4b2e03900c249308cebb0375167f755
Dian <dianlestari330@gmail.com>
2017-05-24 14:20:14 +0700

    REVISI

--- administrator/view/BerandaUI.php
+++ administrator/view/BerandaUI.php
@@ -9,6 +9,7 @@ class BerandaUI extends View
 	
 	public function tampilLengkap()
 	{
+		//i
 		include_once 'pages/beranda.php';
 
 		$this->end();//this funsinya untuk meghentkan fungsi di kelasnya dan fungsi end ada di view
--- view/UtamaUI.php
+++ view/UtamaUI.php
@@ -9,27 +9,16 @@ class UtamaUI extends View
 	
 	public function tampilkanBerita()
 	{
+		include_once 'model/Berita.php';
+		$brt = new Berita();
+
+		$isi_berita = $brt->ambilBerita();
 		include_once 'pages/beranda.php';
 		$this->end();//this funsinya untuk meghentkan fungdi di kelasnya dan fungsi end ada di view
 	}
 }
 
 //modifikasi utama UI.php dengan memanggil class berita
-class BeritaUI extends View
-{
-	
-	public function tampilBerita()
-	{
-		include_once 'model/Berita.php';
-
-		$brt = new Berita();
-
-		$isi_berita = $brt->ambilBerita();
-
-		include_once 'pages/listberita.php';
-		$this->end();
-	}
-}
 
 
 
